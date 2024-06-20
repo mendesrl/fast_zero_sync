cccb3a0ca7ebc374a9552d4bc146ce651f63cdbe
mendesrl <ribeiro.larissamendes@gmail.com>
2024-06-20 19:09:09 -0400

    chore: aula 02

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,27 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/laris')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Hello, World!'}
+
+
+@app.get('/html', response_class=HTMLResponse)
+def read_html():
+    return """
+    <html>
+      <head>
+        <title> Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html>
+"""
