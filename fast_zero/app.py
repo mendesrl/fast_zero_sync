5bfe9f5705aa463e0a458070b46cee1211a93d78
mendesrl <ribeiro.larissamendes@gmail.com>
2024-06-21 08:14:00 -0400

    aula 03

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -3,15 +3,23 @@ from http import HTTPStatus
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
-from fast_zero.schemas import Message
+from fast_zero.schemas import Message, UserDB, UserPublic, UserSchema
 
 app = FastAPI()
+
+DATABASE = []
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Hello, World!'}
 
+
+# Exercicio 1
+# - Crie um endpoint que retorna "olá mundo" usando HTML
+# - Escreva seu teste
+# - Dica: para capturar a resposta do HTML do cliente de testes,
+#   você pode usar response.text
 
 @app.get('/html', response_class=HTMLResponse)
 def read_html():
