cb6742d24660564e40a514751394d5d9c96a7175
mendesrl <ribeiro.larissamendes@gmail.com>
2024-06-20 23:18:16 -0400

    exercicio1: add test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,38 @@
 from http import HTTPStatus
 
+import pytest
 from fastapi.testclient import TestClient
 
 from fast_zero.app import app
 
 
-def test_read_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)  # Arrange (Organizar/Preparar)
+@pytest.fixture()
+def client():
+    return TestClient(app)  # Arrange (Organizar/Preparar)
 
-    response = client.get('/laris')  # Act (Agir)
+
+def test_read_root_deve_retornar_ok_e_ola_mundo(client):
+    response = client.get('/')  # Act (Agir)
 
     assert response.status_code == HTTPStatus.OK  # Assert (Afirme)
     assert response.json() == {'message': 'Hello, World!'}  # Assert (Afirme)
+
+
+def test_read_html(client):
+    response = client.get('/html')  # Act (Agir)
+
+    assert response.status_code == HTTPStatus.OK  # Assert (Afirme)
+    assert (
+        response.text
+        == """
+    <html>
+      <head>
+        <title> Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html>
+"""
+    )  # Assert (Afirme)
+
