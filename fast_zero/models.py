532cabb51108094975f5081a27432944d52f0c5b
mendesrl <ribeiro.larissamendes@gmail.com>
2024-07-02 09:55:50 -0400

    Exericio 04 - adicionando um novo campo na tabela

--- fast_zero/models.py
+++ fast_zero/models.py
@@ -21,3 +21,20 @@ class User:
     created_at: Mapped[datetime] = mapped_column(
         init=False, server_default=func.now()
     )
+
+    # Exercícios
+    # Fazer uma alteração no modelo (tabela User)
+    # e adicionar um campo chamado updated_at:
+    # Esse campo deve ser mapeado para o tipo datetime
+    # Esse campo não deve ser inicializado por padrão init=False
+    # O valor padrão deve ser now
+    # Toda vez que a tabela for atualizada esse campo deve ser atualizado:
+
+    # mapped_column(onupdate=func.now())
+    # Criar uma nova migração autogerada com alembic
+    # Aplicar essa migração ao banco de dados
+    # alembic revision --autogenerate -m "add updated_at column to users"
+    # alembic upgrade head
+    updated_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now(), onupdate=func.now()
+    )
