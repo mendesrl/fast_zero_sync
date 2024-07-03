12122919b57c52726add68349e41714ec7b54c79
mendesrl <ribeiro.larissamendes@gmail.com>
2024-07-02 20:13:47 -0400

    Atualizando endpoints para usar o banco de dados real

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,59 +1,75 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy import select
+from sqlalchemy.orm import Session
 
+from fast_zero.database import get_session
+from fast_zero.models import User
 from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
 app = FastAPI()
 
 DATABASE = []
 
-# @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
-# def read_root():
-#     return {'message': 'Hello, World!'}
+
+@app.get('/users/', response_model=UserList)
+def read_users(
+    limit: int = 1, offset: int = 0, session: Session = Depends(get_session)
+):
+    user = session.scalars(select(User).limit(limit).offset(offset))
+    return {'users': user}
 
 
-# # Exercicio 1
-# # - Crie um endpoint que retorna "olá mundo" usando HTML
-# # - Escreva seu teste
-# # - Dica: para capturar a resposta do HTML do cliente de testes,
-# #   você pode usar response.text
+# Exercícios
+# Criar um endpoint de GET para pegar um
+# único recurso como users/{id} e fazer seus testes.
 
 
-# @app.get('/html', response_class=HTMLResponse)
-# def read_html():
-#     return """
-#     <html>
-#       <head>
-#         <title> Nosso olá mundo!</title>
-#       </head>
-#       <body>
-#         <h1> Olá Mundo </h1>
-#       </body>
-#     </html>
-# """
-
-
-# Recurso de user (manipular os dados de um usuario CRUD)
+@app.get('/user/{user_id}', response_model=UserPublic)
+def read_user_id(user_id: int):
+    if user_id > len(DATABASE) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail='User not found :( ',
+        )
+    return DATABASE[user_id - 1]
 
 
 @app.post('/user/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_user(user: UserSchema):
-    user_with_id = UserDB(**user.model_dump(), id=len(DATABASE) + 1)
-    DATABASE.append(user_with_id)
+def create_user(user: UserSchema, session: Session = Depends(get_session)):
+    db_user = session.scalar(
+        select(User).where(
+            (User.username == user.username) | (User.email == user.email)
+        )
+    )
 
-    return user_with_id
+    if db_user:
+        if db_user.username == user.username:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Username already exists',
+            )
+        elif db_user.email == user.email:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Email already exists',
+            )
 
+    db_user = User(
+        username=user.username, password=user.password, email=user.email
+    )
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
 
-@app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
-def read_users():
-    return {'users': DATABASE}
+    return db_user
 
 
 @app.put(
     '/user/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
 )
-def update_user(user_id: int, user: UserSchema):
+def update_user(user_id: int, user: UserSchema, session=Depends(get_session)):
     if user_id < 1 or user_id > len(DATABASE):
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND,
@@ -74,18 +90,3 @@ def delete_user(user_id: int):
         )
     del DATABASE[user_id - 1]
     return {'message': 'User deleted successfully!'}
-
-
-# Exercícios
-# Criar um endpoint de GET para pegar um
-# único recurso como users/{id} e fazer seus testes.
-
-
-@app.get('/user/{user_id}', response_model=UserPublic)
-def read_user_id(user_id: int):
-    if user_id > len(DATABASE) or user_id < 1:
-        raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND,
-            detail='User not found :( ',
-        )
-    return DATABASE[user_id - 1]
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,30 +1,5 @@
 from http import HTTPStatus
 
-# def test_read_root_deve_retornar_ok_e_ola_mundo(client):
-#     response = client.get('/')  # Act (Agir)
-
-#     assert response.status_code == HTTPStatus.OK  # Assert (Afirme)
-#     assert response.json() == {'message': 'Hello, World!'}  # Assert (Afirme)
-
-
-# def test_read_html(client):
-#     response = client.get('/html')  # Act (Agir)
-
-#     assert response.status_code == HTTPStatus.OK  # Assert (Afirme)
-#     assert (
-#         response.text
-#         == """
-#     <html>
-#       <head>
-#         <title> Nosso olá mundo!</title>
-#       </head>
-#       <body>
-#         <h1> Olá Mundo </h1>
-#       </body>
-#     </html>
-# """
-#     )  # Assert (Afirme)
-
 
 def test_create_user(client):
     response = client.post(
